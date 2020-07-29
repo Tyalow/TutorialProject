086076c4f5acb5ae691c64700cbe49164cd5c78f
Tyler O <Tyalowx@gmail.com>
2020-07-28 19:54:56 -0600

    Comment Change

--- TutorialProject/Main.cpp
+++ TutorialProject/Main.cpp
@@ -10,14 +10,14 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-//Vertex shader source code in a string
+//Vertex shader source code (GLSL)
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
-//Fragment shader source code in a string
+//Fragment shader source code (GLSL)
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
