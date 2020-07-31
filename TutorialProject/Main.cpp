520b731ed4073211474974afc20c93e6d60a8c67
Tyler O <Tyalowx@gmail.com>
2020-07-30 21:41:16 -0600

    Cleanup

--- TutorialProject/Main.cpp
+++ TutorialProject/Main.cpp
@@ -6,6 +6,23 @@
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
+"}\n\0";
+
 //Settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -85,7 +102,7 @@ int main()
 
     //Triangle vertices for EBO
     float vertices[] = {
-     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top rightf
      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
     -0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 0.0f  // top left 
