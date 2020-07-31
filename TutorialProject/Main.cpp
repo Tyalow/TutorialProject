4057fefb2c22ff85af2149983a7ebf09b6dcc742
Tyler O <Tyalowx@gmail.com>
2020-07-30 21:08:58 -0600

    Test Push

--- TutorialProject/Main.cpp
+++ TutorialProject/Main.cpp
@@ -10,27 +10,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-//Vertex shader source code (GLSL)
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-//Fragment shader source code (GLSL)
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(ourColor, 1.0);\n"
-"}\0";
-
-
-
 int main()
 {
     glfwInit();
